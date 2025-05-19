bf2b20116df63489e676049e69d246a626c7c4c4
Itachi <vedantlamba18@gmail.com>
2025-05-19 22:41:54 +0530

    starting from here, and making it ready for deployment

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen border flex justify-center items-center text-2xl md:text-3xl">
+      Myzen – Learn. Grow. Evolve. Welcome to Myzen, a next-generation Learning
+      Management System built to empower learners and educators alike. Whether
+      you're a student looking to sharpen your skills or an instructor ready to
+      share your knowledge, Myzen gives you the tools to succeed in the modern
+      learning era.
+    </div>
+  );
+};
+
+export default HomePage;
