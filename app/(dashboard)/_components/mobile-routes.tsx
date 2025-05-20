4024632a20bef4655ad7469a1a40c25bf23e9c86
Itachi <vedantlamba18@gmail.com>
2025-05-20 10:37:08 +0530

    styled in menu and mob nav

--- app/(dashboard)/_components/mobile-routes.tsx
+++ app/(dashboard)/_components/mobile-routes.tsx
@@ -7,7 +7,7 @@ import { TeacherBtn } from "./teacher-btn";
 
 export const MobileRoutes = () => {
   return (
-    <div className="w-[90%] md:w-[80%] mx-auto flex justify-between items-center">
+    <div className="px-4 md:px-0 w-full md:w-[80%] flex justify-end items-center">
       {/* On desktop: TeacherBtn left, others right. On mobile: all right. */}
 
       <SessionProvider>
