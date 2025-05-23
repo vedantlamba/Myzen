57677892e21a817950ab7b672947f06b74acda28
Itachi <vedantlamba18@gmail.com>
2025-05-23 15:34:53 +0530

    starting with working with api routes of next js

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 
 const CoursesPage = () => {
   return (
     <div className="h-full flex justify-center items-center">
       <Link href="/teacher/create">
-        <Button className="cursor-pointer">New Course</Button>
+        <Button className="cursor-pointer">
+          <Plus />
+          New Course
+        </Button>
       </Link>
     </div>
   );
---
+++ app/api/courses/route.ts
@@ -0,0 +1,27 @@
+import { auth } from "@/auth";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const session = await auth();
+    const userId = session?.user?.id;
+    const { title } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
