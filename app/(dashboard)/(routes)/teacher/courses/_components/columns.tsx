b825029a50c0264786ccb62f00fa2552026c58de
Itachi <vedantlamba18@gmail.com>
2025-05-25 20:59:13 +0530

    added the data table for course creator

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Course } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react";
+import Link from "next/link";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+
+export const columns: ColumnDef<Course>[] = [
+  {
+    accessorKey: "title",
+
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price")) || 0;
+      const formattedPrice = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(price);
+
+      return <div className="font-medium tracking-wider">{formattedPrice}</div>;
+    },
+  },
+  {
+    accessorKey: "isPublished",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const isPublished = row.getValue("isPublished") || false;
+      return (
+        <Badge variant={isPublished ? "published" : "destructive"}>
+          {isPublished ? "Published" : "Draft"}
+        </Badge>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <span className="sr-only">Open Menu</span>
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" />
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,16 +1,62 @@
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
 import Link from "next/link";
+import { Cat } from "react-kawaii";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { Nanum_Pen_Script } from "next/font/google";
+// import { Noto_Sans_JP } from "next/font/google";
 
-const CoursesPage = () => {
+// const notoJP = Noto_Sans_JP({
+//   subsets: ["latin"],
+//   weight: ["400", "500", "700"],
+//   display: "swap",
+// });
+
+const NanumPen = Nanum_Pen_Script({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const CoursesPage = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="flex flex-col justify-start h-full mx-auto items-center max-w-7xl w-full">
+      <div className="relative">
+        <Cat size={240} mood="blissful" color="#FFFDF6" />
+        <div className="absolute top-1/2 left-[80%] -translate-y-1/2 mt-5">
+          <p className={`${NanumPen.className}`}>
+            Hello There!
+          </p>
+        </div>
+      </div>
+
       <Link href="/teacher/create">
-        <Button className="cursor-pointer">
-          <Plus />
-          New Course
-        </Button>
+        <Button className="cursor-pointer">New Course</Button>
       </Link>
+      <div className="w-full">
+        <div className="mx-auto overflow-x-auto py-10 px-2 sm:px-4 md:px-6 text-start">
+          <DataTable columns={columns} data={courses} />
+        </div>
+      </div>
     </div>
   );
 };
