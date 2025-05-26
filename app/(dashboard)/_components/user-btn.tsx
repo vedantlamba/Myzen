7f3008b55e65e3d8a31f1fa612e5b5376e36fea9
Itachi <vedantlamba18@gmail.com>
2025-05-26 17:51:55 +0530

    tweaks in user-btn, applied a new font and fixed sign out btn

--- app/(dashboard)/_components/user-btn.tsx
+++ app/(dashboard)/_components/user-btn.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { ClipLoader } from "react-spinners";
 import Link from "next/link";
+import { Nanum_Pen_Script } from "next/font/google";
+const NanumPen = Nanum_Pen_Script({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const UserBtn = ({ greeting }: { greeting?: string }) => {
   const { data: session, status } = useSession();
 
   return (
     <div>
-      <div className="flex justify-center items-center h-full px-3 text-center min-w-[150px] max-w-[250px] truncate">
+      <div className="flex flex-col justify-center items-center h-full px-3 text-center min-w-[150px] max-w-[250px] truncate">
         {!session ? (
           <Link href="/auth/login">
             {status === "loading" ? (
@@ -24,11 +30,20 @@ export const UserBtn = ({ greeting }: { greeting?: string }) => {
             )}
           </Link>
         ) : (
-          <span className="text-[13px] md:text-[15px] font-medium truncate">
-            {greeting}
+          <span className="text-[13px] md:text-[15px] font-medium truncate flex flex-col">
+            <span className={`${NanumPen.className} text-lg`}>{greeting}</span>
             {session.user?.name}
           </span>
         )}
+        <div className="md:hidden mt-3">
+          {session ? (
+            <Button onClick={() => signOut({ callbackUrl: "/auth/login" })}>
+              Sign Out
+            </Button>
+          ) : (
+            <span></span>
+          )}
+        </div>
       </div>
     </div>
   );
